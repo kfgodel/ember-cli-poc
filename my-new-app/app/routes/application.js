59f4db1aade4cdd7f98f2c2cfb3231b72e3428fd
Dario L Garcia <kfgodel@gmail.com>
2015-11-08 19:32:57 -0600

    Default index route

--- my-new-app/app/routes/application.js
+++ my-new-app/app/routes/application.js
@@ -21,6 +21,20 @@ export default Ember.Route.extend({
         console.log(transition);
         return this._super(error, transition);
       }
+    },
+    logout: function() {
+      var self = this;
+      Ember.$.post("/j_logout", {}).then(
+        function(response) {
+          var loginController = self.controllerFor('login');
+          loginController.set('authenticated', null);
+          self.transitionTo('login');
+        },
+        function(response){
+          console.log("Error logging out");
+          console.log(response);
+        }
+      );
     }
   }
 });
