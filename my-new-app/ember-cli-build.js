ce2c8e209c5fe7fb4b103b7c31e736103bf807ea
Dario L Garcia <kfgodel@gmail.com>
2016-02-23 20:05:56 -0300

    DLG-2289 #comment Markdown component extracted as addon #resolve

--- my-new-app/ember-cli-build.js
+++ my-new-app/ember-cli-build.js
@@ -21,7 +21,6 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-  app.import('bower_components/marked/marked.min.js');
 
   return app.toTree();
 };
