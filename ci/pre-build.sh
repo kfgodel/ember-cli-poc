d86f64b20cdcdae3fb095672a45ebf4e2b3f39e7
Dario L Garcia <kfgodel@gmail.com>
2016-03-24 16:49:39 -0300

    Update dependencies before trying to run

--- ci/pre-build.sh
+++ ci/pre-build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 cd procedimientos-activos
+npm install
+bower install
 ember build --environment=production
+
+
+# Prerequisitos
+# wget -qO- https://deb.nodesource.com/setup_4.x | sudo bash -
+# sudo apt-get install -y nodejs
+# sudo apt-get install -y build-essential
+# sudo npm install -g bower
+# sudo npm install -g ember-cli
