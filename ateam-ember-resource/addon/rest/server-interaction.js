777755abd82a2bd44fabbc0842831b29779a6021
Dario L Garcia <kfgodel@gmail.com>
2016-09-11 19:29:18 -0300

    Add tests in mocha for some components

--- ateam-ember-resource/addon/rest/server-interaction.js
+++ ateam-ember-resource/addon/rest/server-interaction.js
@@ -1,47 +1,73 @@
+/**
+ * This class represents a backend interaction in which the error cause can be discriminated as an authentication problem
+ *
+ * @param requestPromise The request promise to decorate
+ */
 export default function ServerInteraction(requestPromise){
-  var handlers;
-  // PUBLIC
+  var _handlers;
+  /**
+   * Defines a callback for sucess handling
+   * @param action The function that will receive the backend response as an object
+   */
   this.whenSucceeded = function(action){
-    handlers.successHandler = action;
+    _handlers.successHandler = action;
     return this;
   };
+  /**
+   * Defines the callback to call after a needed authentication and allows retrying the failed action (or something else)
+   * @param action The function that will get called after a successful reauthentication (no arg)
+   */
   this.whenInterruptedAndReauthenticated = function(action){
-    handlers.reauthenticationHandler = action;
+    _handlers.reauthenticationHandler = action;
     return this;
   };
+  /**
+   * Defines a callback to be used when the request is rejected as unatithenticated (401).
+   * This callback receives a function as an argument to be called after a succesful authentication
+   * @param action The function to be called when unauthenticated to authenticate (or handle the error)
+   */
   this.whenUnauthorized = function(action){
-    handlers.authenticationHandler = action;
+    _handlers.authenticationHandler = action;
     return this;
   };
+  /**
+   * Defines the callback to use when the requests fails due to an unknown error
+   * @param action The function that receives the response as an argument
+   */
   this.whenFailed = function(action){
-    handlers.errorHandler = action;
+    _handlers.errorHandler = action;
     return this;
   };
 
   // PRIVATE
-  var defaultHandler = function(value){return value;};
-  handlers = {
-    successHandler: defaultHandler,
-    authenticationHandler: defaultHandler,
-    reauthenticationHandler: defaultHandler,
-    errorHandler: defaultHandler,
+  var _defaultHandler = function (value) {
+    return value;
+  };
+  _handlers = {
+    successHandler: _defaultHandler,
+    authenticationHandler: _defaultHandler,
+    reauthenticationHandler: _defaultHandler,
+    errorHandler: _defaultHandler,
   };
 
-  var successfulRequestHandler = function(value){
-    return handlers.successHandler(value);
+  var _successfulRequestHandler = function (value) {
+    return _handlers.successHandler(value);
   };
-  var failedRequestHandler = function(response){
+  var _failedRequestHandler = function (response) {
     var statusCode = response.status;
     if(statusCode === 401){
-      handlers.authenticationHandler(handlers.reauthenticationHandler);
+      _handlers.authenticationHandler(_handlers.reauthenticationHandler);
     }else{
-      handlers.errorHandler();
+      _handlers.errorHandler();
     }
   };
 
-  var interactionPromise = requestPromise.then(successfulRequestHandler, failedRequestHandler);
-  // Make this instance appear as a promise
+  var _interactionPromise = requestPromise.then(_successfulRequestHandler, _failedRequestHandler);
+
+  /**
+   * Method implemented to be a valid ember promise
+   */
   this.then = function(succes, error){
-    return interactionPromise.then(succes, error);
+    return _interactionPromise.then(succes, error);
   };
 }
