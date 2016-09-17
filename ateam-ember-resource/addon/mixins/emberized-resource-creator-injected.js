5e5704155d3e24cdab3615991b7ee8e3208037f6
Dario L Garcia <kfgodel@gmail.com>
2016-09-17 20:45:24 -0300

    Fix + Release 0.0.14

--- ateam-ember-resource/addon/mixins/emberized-resource-creator-injected.js
+++ ateam-ember-resource/addon/mixins/emberized-resource-creator-injected.js
@@ -1,7 +1,7 @@
 import Ember from "ember";
 
 export default Ember.Mixin.create({
-  _emberResourceCreator: Ember.inject.service('ember-resource-creator'),
+  _emberResourceCreator: Ember.inject.service('emberized-resource-creator'),
   resourceCreator(){
     return this.get('_emberResourceCreator');
   }
