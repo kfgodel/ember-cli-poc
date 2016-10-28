21a167befa8516c8b306b9fe570f5292a2cea5ee
Dario L Garcia <dario.garcia@10pines.com>
2016-10-27 22:39:41 -0300

    Ejemplo para unificar comm entre fron y back en un solo endpoint

--- procedimientos-activos/app/routes/procedures/filter.js
+++ procedimientos-activos/app/routes/procedures/filter.js
@@ -1,6 +1,7 @@
 import Ember from "ember";
 import AuthenticatedRoute from "ateam-ember-authenticator/mixins/authenticated-route";
 import ProcedureServiceInjected from "../../mixins/procedure-service-injected";
+import MessageServiceInjected from "../../mixins/message-service-injected";
 import SearcherInjected from "../../mixins/searcher-injected";
 import MessagerInjected from "ateam-ember-messager/mixins/messager-injected";
 import ProcedureSearchStarted from "../../messages/procedure-search-started";
@@ -8,7 +9,7 @@ import ProcedureSearchStopped from "../../messages/procedure-search-stopped";
 import NavigatorInjected from "../../mixins/navigator-injected";
 
 
-export default Ember.Route.extend(AuthenticatedRoute, ProcedureServiceInjected, SearcherInjected, MessagerInjected, NavigatorInjected, {
+export default Ember.Route.extend(AuthenticatedRoute, ProcedureServiceInjected, SearcherInjected, MessagerInjected, NavigatorInjected, MessageServiceInjected, {
   queryParams:{
     filterText:{
       refreshModel: true,  // Refresh the model whenever the query param changes
@@ -19,9 +20,13 @@ export default Ember.Route.extend(AuthenticatedRoute, ProcedureServiceInjected, 
     // Because the queyParam is not available to the searcher we let it know its value (needed when this route is accessed by url)
     this.communicateQueryParamToSearcher(filterText);
 
-    return this.promiseWaitingFor(this.procedureService().getAllProceduresMathing(filterText))
+    var messageContent = Ember.Object.create({searchText: ''});
+    return this.promiseWaitingFor(this.messageService().sendMessage(messageContent))
       .whenInterruptedAndReauthenticated(()=>{
         this.navigator().navigateToProceduresListFilteringBy(filterText);
+      })
+      .then((wrap)=> {
+        return wrap.get('list');
       });
   },
   actions: {
---
+++ procedimientos-activos/app/services/message-service.js
@@ -0,0 +1,30 @@
+import Ember from "ember";
+import RequesterServiceInjected from "ateam-ember-resource/mixins/requester-service-injected";
+import ResourceLocatorInjected from "ateam-ember-resource/mixins/resource-locator-injected";
+import ResultEmberizer from "ateam-ember-resource/utils/result-emberizer";
+
+/**
+ * Esta clase permite comunicarse con el backend a travez de un solo endpoint (sin utilizar la convecion rest)
+ */
+export default Ember.Service.extend(RequesterServiceInjected, ResourceLocatorInjected, {
+
+  sendMessage(messageContent) {
+    var requestArgument = {
+      method: "POST",
+      url: this._resourceUrl(),
+      data: JSON.stringify(messageContent)
+    };
+    var promise = this.requesterService().makeRequest(requestArgument);
+    return this._emberizer().emberizing(promise);
+  },
+
+  // PRIVATE
+  _resourceUrl: function () {
+    var resourceName = 'messages';
+    return this.resourceLocator().resourceUrl(resourceName);
+  },
+  _emberizer(){
+    return ResultEmberizer.create();
+  },
+
+});
